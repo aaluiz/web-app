0fbea5efbeb7d6ec3e60327523234b014eceda28
Alan Alves Luiz <alan.luiz@creasp.org.br>
2022-01-31 22:30:03 -0300

    tailwind css on fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,33 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavigationComponent } from './shared/components/navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-    declarations: [AppComponent, LayoutComponent, LoginComponent, HomeComponent],
+    declarations: [AppComponent, LayoutComponent, LoginComponent, HomeComponent, NavigationComponent],
     imports: [
         BrowserModule.withServerTransition({ appId: 'serverApp' }),
         AppRoutingModule,
         BrowserAnimationsModule,
+        MatGridListModule,
+        MatCardModule,
+        MatMenuModule,
+        MatIconModule,
+        MatButtonModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+    mode: 'aot',
+    purge: {
+      enabled: process.env.NODE_ENV === 'production',
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
     content: ['./src/**/*.{html,ts}'],
     theme: {
         extend: {},
